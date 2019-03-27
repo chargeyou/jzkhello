2f28e4a936c69fa0a8bacc2b9836475db4b9ea3d
chargeyou <760382275@qq.com>
2019-03-27 15:04:04 +0800

    qa

---
+++ src/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Hello World
+ * @author: jasonj
+ * @website: ***
+ * @date: 2018-10-09
+ */
+
+namespace HelloWorld;
+
+class HelloWorld {
+
+    public function say_hello()
+    {
+        return "hello world";
+    }
+}
